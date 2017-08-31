982e55cde1d2b126d627518aeba1ff2de6ac16f9
andresr5 <30513328+andresr5@users.noreply.github.com>
2017-08-31 18:11:21 -0500

    Pruebas de Operaciones

--- src/test/PruebaOperaciones.java
+++ src/test/PruebaOperaciones.java
@@ -7,10 +7,38 @@ import com.calculadora.servicesImpl.Invoker;
 
 public class PruebaOperaciones {
   @Test
-  public void pruebaControlOperacionSinPrecedencia() {
+  public void pruebaSuma() {
 	  Invoker calculadoraFisica = new Invoker();
 	  OperacionController oc=new OperacionController();
-	  double resultado=oc.operarSinPrecedencia(calculadoraFisica, "3+2");
-	  Assert.assertEquals(resultado,5.0);
+	  double resultado=oc.operarSegunSigno(1, 2, "+");
+	  Assert.assertEquals(resultado,3.0);
+  }
+  @Test
+  public void pruebaResta() {
+	  Invoker calculadoraFisica = new Invoker();
+	  OperacionController oc=new OperacionController();
+	  double resultado=oc.operarSegunSigno(2, 1, "-");
+	  Assert.assertEquals(resultado,1.0);
+  }
+  @Test
+  public void pruebaMultiplicacion() {
+	  Invoker calculadoraFisica = new Invoker();
+	  OperacionController oc=new OperacionController();
+	  double resultado=oc.operarSegunSigno(1, 2, "*");
+	  Assert.assertEquals(resultado,2.0);
+  }
+  @Test
+  public void pruebaDivision() {
+	  Invoker calculadoraFisica = new Invoker();
+	  OperacionController oc=new OperacionController();
+	  double resultado=oc.operarSegunSigno(2, 1, "/");
+	  Assert.assertEquals(resultado,2.0);
+  }
+  @Test
+  public void pruebaPotencia() {
+	  Invoker calculadoraFisica = new Invoker();
+	  OperacionController oc=new OperacionController();
+	  double resultado=oc.operarSegunSigno(2, 2, "^");
+	  Assert.assertEquals(resultado,4.0);
   }
 }
